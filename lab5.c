364fa071e3f1375e768eb2979745f7991a8b07c4
Anastasia Korshunova <165956434+AnastasiaKorshunova@users.noreply.github.com>
2025-05-26 13:11:05 +0300

    first

---
+++ lab5.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+typedef struct {
+    int num_processes;
+    int exec_index;
+    char **exec_args;
+} Args;
+
+void parse_arguments(int argc, char *argv[], Args *args);
+void create_children(Args *args, pid_t *children);
+void child_loop(int index);
+void run_exec(char **exec_args);
+void terminate_children(pid_t *children, int count);
+void wait_for_children(pid_t *children, int count);
+
+
+void parse_arguments(int argc, char *argv[], Args *args) {
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <num_processes> <exec_index> <command> [args...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    args->num_processes = atoi(argv[1]);
+    args->exec_index = atoi(argv[2]);
+
+    if (args->exec_index < 0 || args->exec_index >= args->num_processes) {
+        fprintf(stderr, "Invalid exec_index: %d (must be from 0 to %d)\n", args->exec_index, args->num_processes - 1);
+        exit(EXIT_FAILURE);
+    }
+
+    args->exec_args = &argv[3];
+}
+
+void create_children(Args *args, pid_t *children) {
+    for (int i = 0; i < args->num_processes; i++) {
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            if (i == args->exec_index) {
+                run_exec(args->exec_args);
+            } else {
+                child_loop(i);
+            }
+        } else {
+            children[i] = pid;
+        }
+    }
+}
+
+void child_loop(int index) {
+    while (1) {
+        printf("Child #%d (PID %d) is working...\n", index, getpid());
+        sleep(1);
+    }
+}
+
+void run_exec(char **exec_args) {
+    execvp(exec_args[0], exec_args);
+    perror("exec failed");
+    exit(EXIT_FAILURE);
+}
+
+void terminate_children(pid_t *children, int count) {
+    for (int i = 0; i < count; i++) {
+        kill(children[i], SIGKILL);
+        printf("Killed child with PID %d\n", children[i]);
+    }
+}
+
+void wait_for_children(pid_t *children, int count) {
+    for (int i = 0; i < count; i++) {
+        waitpid(children[i], NULL, 0);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    Args args;
+    parse_arguments(argc, argv, &args);
+
+    pid_t *children = malloc(sizeof(pid_t) * args.num_processes);
+    if (!children) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+
+    create_children(&args, children);
+
+    sleep(5);  
+
+    terminate_children(children, args.num_processes);
+    wait_for_children(children, args.num_processes);
+
+    free(children);
+    printf("Parent finished.\n");
+    return 0;
+}
